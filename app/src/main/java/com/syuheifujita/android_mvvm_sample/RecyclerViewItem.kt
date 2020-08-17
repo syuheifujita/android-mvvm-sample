5a3112af4aac7e7c36734cb3020b3da7a00a04e1
syuheifujita <syuheifujita0117@gmail.com>
2020-08-18 08:04:49 +0900

    first

---
+++ app/src/main/java/com/syuheifujita/android_mvvm_sample/RecyclerViewItem.kt
@@ -0,0 +1,14 @@
+package com.syuheifujita.android_mvvm_sample
+
+import com.syuheifujita.android_mvvm_sample.databinding.ItemRecyclerviewBinding
+import com.xwray.groupie.databinding.BindableItem
+
+class RecyclerViewItem(val text: String): BindableItem<ItemRecyclerviewBinding>() {
+    override fun getLayout() = R.layout.item_recyclerview
+
+    override fun bind(viewBinding: ItemRecyclerviewBinding, position: Int) {
+        //viewmodelだとここが違う
+//        viewBinding.text = text
+        viewBinding.textView.setText(text)
+    }
+}
